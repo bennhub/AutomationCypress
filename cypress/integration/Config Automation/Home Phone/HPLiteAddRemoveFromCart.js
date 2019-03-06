eaf13c8dfe1e50298b347ed83855574f5c64cd34
benhubb <benneilson0226@gmail.com>
2019-03-06 13:40:42 -0800

    updated home phone lite tests

--- cypress/integration/Config Automation/Home Phone/HPLiteAddRemoveFromCart.js
+++ cypress/integration/Config Automation/Home Phone/HPLiteAddRemoveFromCart.js
@@ -19,23 +19,28 @@ describe('Home Phone', function() {
       cy.contains('Check availability').click()
       cy.wait(9000)
 
-     // Verify correct image is visible
-      cy.get('.css-11bxvq').should('be.visible');
+     // Verify correct image is visible on pdp page
+      cy.get('.css-lv63rw').should('be.visible');
 
-     // Verify correct pricing and details
-      cy.get('.TDS_Box-modules__betweenRightMargin-1___XT_b2 > div').should('have.text', '$30/month');
-      cy.get('.css-14w0bz > :nth-child(1) > :nth-child(3)').should('have.text', 'Pay $15 for the first 3 months1');
+     // Verify correct pricing and details on pdp page
+      cy.get('.css-14w0bz > :nth-child(1)').should('have.text', 'No term  $30/monthPay $15 for the first 3 months2');
       
+      // I add to cart
       cy.contains('Add to cart').click()
       cy.wait(3000)
 
-      cy.get('.css-1uway3t').click() 
+      // Then click review cart on panel
+      cy.contains('Review cart').click() 
       cy.wait(3000)
 
+      // I Verify correct pricing and text in summary box 
+      cy.get('.summary-box__container').should('have.text', 'SummaryMonthly billFirst billHome Phone$15.00$15.00SubtotalBefore taxes$15.00Monthly bill$15.00First billCheck outContinue shoppingClose');
+       
+      // I remove from cart
       cy.contains('Remove').click()
       cy.wait(3000)
 
-     // Verify removed from cart and action copy
+     // Then verify homephone was removed and correct copy is displayed
       cy.get('.action-error__copy > .TDS_Link-modules__base___1BUGa').should('have.text', 'add items'); 
 
       // clear cookies 
